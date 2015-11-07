089b06add7670206f848cc3d6e4ef27abc395f6d
Austin Eng <213reeses@gmail.com>
2015-11-07 03:08:08 -0500

    fix stuff ugh

--- middlewares/processMetadata.js
+++ middlewares/processMetadata.js
@@ -10,11 +10,12 @@ var processMetadata = function (req, res, next) {
   console.log(req.body);
   var final_results = [];
   var gaps = [];
-  for (guess : req.body){
+  for (var i = 0; i < req.body.length; i++) {
+  	var guess = req.body.data[i];
   	if(guess.results[0].final)
-  		final_results.push(guess.rfinal_results[i][final_results[i].length][0]esults[0].alternatives[0].timestamps);
+  		final_results.push(guess.final_results[i][final_results[i].length][0].results[0].alternatives[0].timestamps);
   }
-  for (int i = 0; i<final_results.length-1; i++){
+  for (var i = 0; i<final_results.length-1; i++){
   		gaps[i] = [final_results[i][final_results[i].length][0], 
   					final_results[i+1][0][0], (final_results[i+1][0][1]-
   					final_results[i][final_results[i].length][2])];
