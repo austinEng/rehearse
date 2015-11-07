993ad33af303e149728c661175ecf254baf76386
Austin Eng <213reeses@gmail.com>
2015-11-06 23:23:48 -0500

    initial code

---
+++ app.js
@@ -0,0 +1,57 @@
+"use strict";
+var logger = require('morgan');
+var lessMiddleware = require('less-middleware');
+var express = require('express');
+var watson = require('watson-developer-cloud');
+var path = require('path');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var config = require('./config.js');
+
+var authService = watson.authorization(config.watson);
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(lessMiddleware(path.join(__dirname, 'source', 'less'), {
+    dest: path.join(__dirname, 'public'),
+    preprocess: {
+        path: function(pathname, req) {
+            return pathname.replace(path.sep + 'stylesheets' + path.sep, path.sep);
+        }
+    }
+}));
+
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/speechanalyzer', function(err) {
+    if(!err) {
+        console.log('Connected to database');
+    } else {
+        return;
+    }
+
+    app.get('/', function(req, res, next) {
+        res.render('index', { ct: req._csrfToken });
+    });
+
+    app.post('/api/token', function(req, res, next) {
+        authService.getToken({url: config.watson.url}, function(err, token) {
+            if (err)
+                next(err);
+            else
+                res.send(token);
+        });
+    });
+
+    app.post('/receivedata', function(req, res, next) {
+        console.log(req);
+    });
+
+    var port = process.env.PORT || 3000;
+    app.listen(port);
+});
+
