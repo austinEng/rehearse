ec6ad4b3e4d29882c68d261bfc29dfe4f02af92c
Raymond Yin <ryin@seas.upenn.edu>
2015-11-07 21:36:45 -0500

    processMetadata.js

--- public/scripts/stats.js
+++ public/scripts/stats.js
@@ -1,4 +1,19 @@
 angular.module('rehearse', []).controller('Stats', function() {
-	var stats = this;
+    // Grab data for the particular user
+	var data = {
+  pauses: 
+   [ [ 'our', 'car', 0 ],
+     [ 'car', 'yet', 0.16000000000000003 ],
+     [ 'yet', 'so', 0 ] ],
+  hesitations: [],
+  wpm: 149.06832298136646,
+  enunciations: 
+   [ [ 'our', 0.35, 0.48 ],
+     [ 'car', 0.48, 0.73 ],
+     [ 'yet', 0.89, 1.16 ],
+     [ 'so', 1.16, 1.61 ] ],
+  sentence: 'our car yet so' };
+    $scope.data = data;
+    var stats = this;
 	
 });
